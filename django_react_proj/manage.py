99cd3bb0a46eb804fce5b3fd2c6e8dfe95db174b
Jacob Rodgers <yudjinncoding@gmail.com>
2020-07-22 23:06:55 -0600

    Test Stack for student tracking

--- django_react_proj/manage.py
+++ django_react_proj/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drnote.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_react_proj.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
